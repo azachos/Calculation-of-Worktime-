19b6c7ae383d7cb3e9b4e92fe152fc44c9d0752e
Alexandros Zachos <alzaxo@gmail.com>
2011-05-30 13:29:42 +0300

    indentation

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<? #comment ?>
 <html>
 
 				<head>
@@ -6,43 +7,43 @@
 								</title>
 									<meta http-equiv="Content-type" content="text/html; charset=utf-8;" />
 								<link rel="stylesheet" type="text/css" href="style.css">
-								<script type="text/javascript" src="scripts.js"></script>
-				</head>
+<script type="text/javascript" src="scripts.js"></script>
+</head>
 
-				<body>
-								<div id="main">
-												<form name="form" action="" method="post">
-																<table align="center">												
-																				<tr><td>Κατηγορία υπαλλήλου:</td><td>												
-																												<select id="category" name="category">
-																																<option selected value="O">Οικοδόμος</option>
-																																<option value="H">Ημερομίσθιος</option>
-																																<option value="M">Μισθωτός</option>
-																												</select>
+<body>
+<div id="main">
+<form name="form" action="" method="post">
+<table align="center">												
+<tr><td>Κατηγορία υπαλλήλου:</td><td>												
+<select id="category" name="category">
+<option selected value="O">Οικοδόμος</option>
+<option value="H">Ημερομίσθιος</option>
+<option value="M">Μισθωτός</option>
+</select>
 
-																												</td><tr>
-																												<tr><td>										
-																																				Επιλογή μήνα:</td><td>
-																																				<select id="month" name="month" onchange="loadGrid();">
-																																								<?
-																																								$month = array ('January','February','March','April','May','June','July','August','September','October','November','December');
-																																								for($lt=0; $lt<12; $lt++) {
-																																								if ($lt+1 == 5	)
-																																								print("<option name='$month[$lt]' value='$lt' selected >$month[$lt]</option> \n");
-																																								else
-																																								print("<option name='$month[$lt]' value='$lt'>$month[$lt]</option> \n");
-																																								}
-																																								?>
-																																				</select>
+</td><tr>
+<tr><td>										
+Επιλογή μήνα:</td><td>
+<select id="month" name="month" onchange="loadGrid();">
+<?
+$month = array ('January','February','March','April','May','June','July','August','September','October','November','December');
+for($lt=0; $lt<12; $lt++) {
+				if ($lt+1 == 5	)
+								print("<option name='$month[$lt]' value='$lt' selected >$month[$lt]</option> \n");
+				else
+								print("<option name='$month[$lt]' value='$lt'>$month[$lt]</option> \n");
+}
+?>
+</select>
 
-																								</td></tr></table>												
+				</td></tr></table>												
 
-																								<input type="submit" name="submit">
+				<input type="submit" name="submit">
 
-																				</form>		
-																				<div id="myDiv"><script type="text/javascript">loadGrid()</script></div>
-																				<div id="otherDiv">dgdg</div>
-																</div>
-												</body>
+				</form>		
+				<div id="myDiv"><script type="text/javascript">loadGrid()</script></div>
+				<div id="otherDiv">dgdg</div>
+				</div>
+				</body>
 
-								</html>
+				</html>
