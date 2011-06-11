9e18a0aea306d62ea0388bb7da4c98fa81201dd1
unknown <alex@.(none)>
2011-06-11 16:48:58 +0300

    updated to new sql

--- test.php
+++ test.php
@@ -57,7 +57,7 @@ fclose($fh);
 #
 $Server = "ALEX-PC\SQLEXPRESS";
 
-$connectionInfo = array( "Database"=>"TIMEATT6");
+$connectionInfo = array( "Database"=>"TIMEATT14");
 //connection to the database
 $dbconn = sqlsrv_connect($Server, $connectionInfo)
   or die("Couldn't connect to SQL Server on $Server");
@@ -75,7 +75,7 @@ $xml = simplexml_load_string($xml_output);
 $getdata = $xml->xpath("/data/id");
 //echo $getdata[0];
 //declare the SQL statement that will query the database
-$query = "execute InputDays4 '" .$xml_output ."'";
+$query = "execute attend.InputDays4 '" .$xml_output ."'";
 
 //execute the SQL query and return records
 $stmt=sqlsrv_query($dbconn, $query);
@@ -86,6 +86,7 @@ $query = "execute myproc '" .$getdata[0] ."'";
 $stmt = sqlsrv_query($dbconn, $query);
 */
 
+/*
 $prev_stmt=$stmt;
 while (sqlsrv_rows_affected($stmt) >0 ) {	//while positive we are fetching messages, -1 means recordset
 //echo "<br>" .sqlsrv_rows_affected($stmt);	
@@ -93,9 +94,9 @@ $prev_stmt=$stmt;			//keep the previou statement
 sqlsrv_next_result($stmt);	//next message
 }
 $result = $prev_stmt;	//load recordset
-
+*/
 //display the results
-if ($result) {
+if ($stmt) {
 	//echo $result;
 	//echo "num rows" .sqlsrv_num_rows($result);
 	?>
@@ -135,6 +136,9 @@ if ($result) {
 				<tr><td>ΥΠΕΡ ΣΑΒΒΑΤΟ 2.10</td></tr>
 				<tr><td>ΚΥΡ/ΑΡΓΙΑ 1.75</td></tr>
 				<tr><td>ΥΠΕΡ ΚΥΡ/ΑΡΓΙΑ</td></tr>
+				<tr><td>ΑΡΓΙΑ</td></tr>
+				<tr><td>ΕΠ. ΑΡΓΙΑ</tr></td>
+				<tr><td>ΑΔΕΙΑ</tr></td>
 				</table>
 			</td>
 	<?
@@ -151,14 +155,42 @@ if ($result) {
 				<tr><td><?if ($row['OT21'] != null) echo $row['OT21'];  else echo 0;?></td></tr>
 				<tr><td><?if ($row['OT175'] != null) echo $row['OT175'];  else echo 0;?></td></tr>
 				<tr><td><?if ($row['OT255'] != null) echo $row['OT255'];  else echo 0;?></td></tr>
+				<tr><td><?if ($row['holiday'] != null) echo $row['holiday'];  else echo 0;?></td></tr>
+				<tr><td><?if ($row['validhol'] != null) echo $row['validhol'];  else echo 0;?></td></tr>
+				<tr><td><?if ($row['dayoff'] != null) echo $row['dayoff'];  else echo 0;?></td></tr>
 				</table>
 			</td>
 	<? } ?>	
 	</tr>
 	</table>
+	</br>
+	<h3>Σύνολα</h3>
+	<table class="sample2">
+	<tr>
+		<td>asf</td>
+		<td>sundays</td>
+		<td>ot12</td>
+		<td>night</td>
+		<td>ot13</td>
+		<td>ot21</td>
+		<td>ot255</td>
+	</tr>
+	<tr>
+	<?	sqlsrv_next_result($stmt);	//next message
+		while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+	?>
+				<td><?if ($row['ASF'] != null) echo $row['ASF'];  else echo 0;?></td>
+				<td><?if ($row['SUNDAYS'] != null) echo $row['SUNDAYS'];  else echo 0;?></td>
+				<td><?if ($row['OT12'] != null) echo $row['OT12'];  else echo 0;?></td>
+				<td><?if ($row['night'] != null) echo $row['night'];  else echo 0;?></td>
+				<td><?if ($row['OT13'] != null) echo $row['OT13'];  else echo 0;?></td>
+				<td><?if ($row['OT21'] != null) echo $row['OT21'];  else echo 0;?></td>
+				<td><?if ($row['OT255'] != null) echo $row['OT255'];  else echo 0;?></td>
+	</tr>
+	</table>
 	</body>
 	</html>
-<?
+<?	}
 } else {
      echo "error";
 }
